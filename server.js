5dd7166d2c9937f6106c458fd014499b963c42ea
aaygun05 <ayguns.abdullah@gmail.com>
2021-04-02 17:31:31 +0300

    make it betterrr

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ socketio.on("connection", socket=>{
 
     function random_rgba() {
         var o = Math.round, r = Math.random, s = 255;
-        return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+        return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s)+')';
     }
 
     let position={
